d341313d827ba67ccaeb3452002d1d377cb17e9b
Moni2005 <mohan243200s@gmail.com>
2024-08-29 19:39:41 +0530

    Brushing up concepts in python

---
+++ collections.py
@@ -0,0 +1,11 @@
+i = 1;
+n = int(input("Enter a number: "))
+def sum():
+    a = int(input("Enter a number: "))
+    b = int(input("Enter a number: "))
+    sum = (a+b)
+    print(sum)
+    return sum;
+while(i<=n):
+    sum()
+    i = i+1
